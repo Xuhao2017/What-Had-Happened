dcddad92659e879dc4b477b0c0f989192a507d6c
Theng <gotawork@163.com>
2012-09-17 21:24:36 +0800

    增加log.h Doxygen注释

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Readme
+    Copyright (c) 2012 Gotaly (gotawork@gmail.com)
+    License: GNU GPLv3
+
+    WhatHadHappend (W2H) 
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.  See the GNU General Public License 
+    for more details.
+
--- src/w2h_log.h
+++ src/w2h_log.h
@@ -1,3 +1,9 @@
+/**
+*    Head file of W2H
+*    Copyright (c) 2012 Gotaly (gotawork@gmail.com)
+*    License: GNU GPLv3
+*/
+
 #ifndef W2H_LOG_H
 #define W2H_LOG_H
 
@@ -9,18 +15,47 @@
 #include "inc.h"
 #include "w2h_lua.h"
 
+/**
+* list of config
+* use two lists to resume config files
+* @key config_class_x class list
+* @key config_id_x class list
+*/
 w2h_list_t *config_class_a;
 w2h_list_t *config_id_a;
 w2h_list_t *config_class_b;
 w2h_list_t *config_id_b;
 
+/**
+* Queue store IO data packet
+*/
 w2h_queue_t *inqueue;
 
+/**
+* a buf point
+*/
 void *mem;
+
 pid_t ch_pid;
 
+/**
+* init w2h_log with config file
+* @param config_file:config file
+* @return 0 on success,negative number when failed 
+*/
 extern  int w2h_init(char *config_file);
+/** 
+* produce a log
+* @param classess : classes name, splited by a space
+* @param id:id name
+* @param fmt:format of message
+* @return 0 on success,negative number when failed 
+*/ 
 extern int w2h_log(char *classes,char *id,char *fmt,...);
+/**
+* finish w2h_log
+* deal with memory and other things
+*/
 extern void w2h_finish();
 
 #endif
